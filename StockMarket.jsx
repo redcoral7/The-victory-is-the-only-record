99cefedd4de7149a4e3b05a434ddcc0f18e342e2
redcoral7 <janesally0227@gmail.com>
2026-03-02 10:00:03 +0900

    Update StockMarket.jsx

--- StockMarket.jsx
+++ StockMarket.jsx
@@ -16,6 +16,7 @@ const StockMarket = ({ user, fetchUserList }) => {
   const checkMarketStatus = () => {
     const now = new Date();
     const hour = now.getHours();
+    return hour >= 9 || hour < 2;
   };
 
   const fetchMarketData = async () => {
